89a4868b1a6b2ad69912cf1775cf5dc6296a140f
Samuel <101745495+bytutscode@users.noreply.github.com>
2024-04-21 23:40:58 -0300

    Update getPost.ts

--- src/controllers/post/getPost.ts
+++ src/controllers/post/getPost.ts
@@ -1,1 +1,7 @@
-removed
+import { Request, Response } from "express";
+import Post from "../../models/Post";
+import View from "../../models/View";
+
+export const getPost = async (req: Request, res: Response) => {
+   
+}
